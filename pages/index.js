31421d7d7f89b064cf6e5ebd2f51bfecc1405d55
smilebotstore <ttes61665@gmail.com>
2025-04-21 13:29:30 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-mport { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import { RotateCcw, Trash2, Send } from 'lucide-react';
 

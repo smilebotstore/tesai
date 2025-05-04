0131eccf65d461f6d41f9792d3fba8c27999c876
smilebotstore <ttes61665@gmail.com>
2025-05-04 12:02:44 +0800

    Update generate-promo.js

--- pages/api/generate-promo.js
+++ pages/api/generate-promo.js
@@ -1,4 +1,4 @@
-import { db } from "@/lib/firebase";
+import { db } from "../../lib/firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 export default async function handler(req, res) {

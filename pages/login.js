bb54c5ef5facd2db6e03dc4ca44309fcace3f298
smilebotstore <ttes61665@gmail.com>
2025-05-06 09:43:06 +0800

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -23,7 +23,7 @@ export default function LoginPage() {
 
       const data = await res.json();
       if (res.status === 200) {
-        alert("Authentication successful");
+        alert(`${isSignUp ? "Sign Up" : "Login"} successful`);
       } else {
         setError(data.error);
       }
@@ -43,105 +43,90 @@ export default function LoginPage() {
       minHeight: "100vh",
       background: "linear-gradient(to bottom, #2196f3 30%, white 30%)",
     }}>
-      {isSignUp && (
-        <div style={{ textAlign: "center", marginBottom: 40 }}>
-          <h2 style={{ color: "white" }}>New here?</h2>
-          <p style={{ color: "white" }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus natus est.
-          </p>
-          <button
-            onClick={() => setIsSignUp(false)}
+      <div style={{ textAlign: "center", marginBottom: 40 }}>
+        <h2 style={{ color: "white" }}>{isSignUp ? "Welcome!" : "New here?"}</h2>
+        <p style={{ color: "white" }}>
+          {isSignUp
+            ? "Create your account to get started."
+            : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio minus natus est."}
+        </p>
+        <button
+          onClick={() => setIsSignUp(!isSignUp)}
+          style={{
+            border: "2px solid white",
+            background: "transparent",
+            color: "white",
+            padding: "10px 25px",
+            borderRadius: "25px",
+            marginTop: "10px",
+            cursor: "pointer"
+          }}
+        >
+          {isSignUp ? "SIGN IN" : "SIGN UP"}
+        </button>
+      </div>
+
+      <div style={{ width: "100%", maxWidth: "400px", background: "white", padding: "30px", borderRadius: "20px", boxShadow: "0 0 10px rgba(0,0,0,0.1)" }}>
+        <h2 style={{ textAlign: "center", marginBottom: "20px" }}>{isSignUp ? "Sign Up" : "Sign In"}</h2>
+        <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
             style={{
-              border: "2px solid white",
-              background: "transparent",
+              padding: "15px",
+              borderRadius: "30px",
+              border: "none",
+              backgroundColor: "#f1f1f1",
+              fontSize: "16px"
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            style={{
+              padding: "15px",
+              borderRadius: "30px",
+              border: "none",
+              backgroundColor: "#f1f1f1",
+              fontSize: "16px"
+            }}
+          />
+          <button
+            type="submit"
+            style={{
+              backgroundColor: "#2196f3",
               color: "white",
-              padding: "10px 25px",
-              borderRadius: "25px",
-              marginTop: "10px",
+              padding: "15px",
+              borderRadius: "30px",
+              border: "none",
+              fontWeight: "bold",
               cursor: "pointer"
             }}
           >
-            SIGN UP
+            {isSignUp ? "SIGN UP" : "LOGIN"}
           </button>
-        </div>
-      )}
+        </form>
 
-      {!isSignUp && (
-        <div style={{ width: "100%", maxWidth: "400px", background: "white", padding: "30px", borderRadius: "20px", boxShadow: "0 0 10px rgba(0,0,0,0.1)" }}>
-          <h2 style={{ textAlign: "center", marginBottom: "20px" }}>Sign In</h2>
-          <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "15px" }}>
-            <input
-              type="email"
-              placeholder="Username"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              style={{
-                padding: "15px",
-                borderRadius: "30px",
-                border: "none",
-                backgroundColor: "#f1f1f1",
-                fontSize: "16px"
-              }}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              style={{
-                padding: "15px",
-                borderRadius: "30px",
-                border: "none",
-                backgroundColor: "#f1f1f1",
-                fontSize: "16px"
-              }}
-            />
-            <button
-              type="submit"
-              style={{
-                backgroundColor: "#2196f3",
-                color: "white",
-                padding: "15px",
-                borderRadius: "30px",
-                border: "none",
-                fontWeight: "bold",
-                cursor: "pointer"
-              }}
-            >
-              LOGIN
-            </button>
-          </form>
-          <p style={{ textAlign: "center", marginTop: "20px" }}>Or Sign in with social platforms</p>
-          <div style={{ display: "flex", justifyContent: "center", gap: "15px", marginTop: "10px" }}>
-            {[...Array(4)].map((_, i) => (
-              <div key={i} style={{
-                width: "50px",
-                height: "50px",
-                borderRadius: "50%",
-                border: "2px solid black"
-              }}></div>
-            ))}
-          </div>
+        <p style={{ textAlign: "center", marginTop: "20px" }}>Or Sign {isSignUp ? "up" : "in"} with social platforms</p>
+        <div style={{ display: "flex", justifyContent: "center", gap: "15px", marginTop: "10px" }}>
+          {[...Array(4)].map((_, i) => (
+            <div key={i} style={{
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+              border: "2px solid black"
+            }}></div>
+          ))}
         </div>
-      )}
-
-      <button
-        onClick={() => setIsSignUp(!isSignUp)}
-        style={{
-          marginTop: "30px",
-          background: "none",
-          border: "none",
-          color: isSignUp ? "white" : "#2196f3",
-          cursor: "pointer",
-          fontSize: "14px"
-        }}
-      >
-        {isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
-      </button>
+      </div>
 
       {error && <p style={{ color: "red", marginTop: 20 }}>{error}</p>}
     </div>
   );
-        }
+              }

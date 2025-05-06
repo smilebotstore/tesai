53b71c02985834b72c9befdf8c7f16285255f310
smilebotstore <ttes61665@gmail.com>
2025-05-06 10:40:44 +0800

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
